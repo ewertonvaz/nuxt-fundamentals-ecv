3a75a210d9e832d2edd543b574fc2692519e7adc
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2021-03-13 11:52:56 -0300

    First commit Nuxt Fundamentals

---
+++ components/Logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <img src="https://vueschool.io/img/favicons/apple-touch-icon.png" alt="the logo">
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    letter-spacing: 1px;
+  }
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+  }
+  .links {
+    padding-top: 15px;
+  }
+</style>
